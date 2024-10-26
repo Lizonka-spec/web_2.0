71be36a939730e8e2213d578df4ae92f066ac82e
Lizonka-spec <proudoffyou01@gmail.com>
2024-10-26 17:41:46 +0300

    feat: add second task

--- tasks/script.js
+++ tasks/script.js
@@ -7,12 +7,14 @@
 // delete user.name;
 
 //2
-// function isEmpty(obj){
-//     for ( let key in obj){
-    
-//     }
+function isEmpty(obj){
+    for ( let key in obj){
+        return false
+    }
+ return true
+}
     
 // 3
-let salaries = { john: 100, Ann: 160, Pete: 130 };
-let sum = (salaries.john + salaries.Pete + salaries.Ann);
-alert(sum);
+// let salaries = { john: 100, Ann: 160, Pete: 130 };
+// let sum = (salaries.john + salaries.Pete + salaries.Ann);
+// alert(sum);
