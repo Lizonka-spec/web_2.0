e78b474aeb9b7e49630da002c88817de4f7b7088
Lizonka-spec <proudoffyou01@gmail.com>
2024-10-26 17:26:08 +0300

    feat: add third task

--- tasks/script.js
+++ tasks/script.js
@@ -1,8 +1,18 @@
-let user = {
-    name: "John",
-    surname: "Smith",
-};
+//1
+// let user = {
+//     name: "John",
+//     surname: "Smith",
+// };
+// user.name = ("Pete");
+// delete user.name;
 
-user.name = ("Pete");
-delete user.name;
-
+//2
+// function isEmpty(obj){
+//     for ( let key in obj){
+    
+//     }
+    
+// 3
+let salaries = { john: 100, Ann: 160, Pete: 130 };
+let sum = (salaries.john + salaries.Pete + salaries.Ann);
+alert(sum);
