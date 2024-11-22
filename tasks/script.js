060f926e84e004cecb62c2e6ab6a0daf84b08ae5
Lizonka-spec <proudoffyou01@gmail.com>
2024-11-23 01:47:49 +0300

    feat: add second task

--- tasks/script.js
+++ tasks/script.js
@@ -1,6 +1,14 @@
 // let arr = prompt('Введите массив строк', );
-function addString(arr=[]){
-    arr.push('Hello');
-    alert(arr);
+// function addString(arr=[]){
+//     arr.push('Hello');
+//     alert(arr);
+// };
+// addString(arr=[]);
+ 
+function addArr(arr=[a]){
+    let result = 0;
+    for(let number of arr){
+        result += number;
+    }
+    alert(result);
 };
-addString(arr=[])
