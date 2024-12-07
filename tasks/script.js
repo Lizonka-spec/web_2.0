cede9095446907ff72155ce5cb23d66fea7a3a93
Lizonka-spec <proudoffyou01@gmail.com>
2024-12-07 09:46:27 +0300

    feat: add second task

--- tasks/script.js
+++ tasks/script.js
@@ -1,9 +1,20 @@
-let map = new Map();
-map.set("Liza", "1");
-map.set("Dasha", "2");
-alert(map.get("Liza"));
-map.delete("Dasha");
-alert(map.has("Dasha"));
-for (let name of map ){
-    alert(name);
-}
+// let map = new Map();
+// map.set("Liza", "1");
+// map.set("Dasha", "2");
+// alert(map.get("Liza"));
+// map.delete("Dasha");
+// alert(map.has("Dasha"));
+// for (let name of map ){
+//     alert(name);
+// }
+
+let set = new Set();
+set.add("Лиза");
+set.add("Саша");
+set.add("Даша");
+set.delete("Лиза");
+alert(set.has("Саша"));
+console.log(set);
+set.forEach((element, elementAgain, set) => {
+   console.log(element);
+});
